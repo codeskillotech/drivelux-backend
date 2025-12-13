6320042370c4f23e5fab4ff389b47ef6298cd437
Biswajit Behera <biswajit0905@gmail.com>
2025-12-13 11:28:38 +0530

    Initial DriveLux backend setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# drivelux-backend
---
+++ index.js
@@ -0,0 +1,30 @@
+// src/index.js
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+
+
+dotenv.config();
+
+const app = express();
+
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // to parse JSON body
+
+// Routes
+
+// Health check
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 5004;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
